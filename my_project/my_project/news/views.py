9fd6c0ece0a958d1c73f5624350fa61211bd24ff
vineeth <vineethpalatty@gmail.com>
2019-08-27 16:07:05 +0530

    first commit

---
+++ my_project/my_project/news/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+import requests
+from django.contrib.auth.decorators import login_required
+@login_required
+# Create your views here.
+def news(request):
+    url = 'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=f9454a944cd14fdea4fb920d5a5378af'
+
+    news_data=[]
+    r = requests.get(url).json()
+    print(r)
+
+    for  i in range(6):
+        city_news = \
+            {
+    #           'author' : r['articles'][0]['author'],
+    #            'title': r['articles'][0]['title'],
+                'description' : r['articles'][i]['description'],
+                'image' : r['articles'][i]['urlToImage'],
+                   'url' : r['articles'][i]['url']
+            }
+        news_data.append(city_news)
+    #
+    context = {
+
+                'news_data' : news_data
+              }
+
+
+    return render(request,'news.html',context)
+
