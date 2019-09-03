726fa383dac6fe4cd3df4c49a68bd65179154b6f
vineeth <vineethpalatty@gmail.com>
2019-09-03 10:09:52 +0530

    final3

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'my_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['my_project/my_project/templates'],
+        'DIRS': (PROJECT_PATH + '/templates/',),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL= 'http://127.0.0.1:8000/news/'
+LOGIN_REDIRECT_URL= 'news'
 LOGOUT_REDIRECT_URL = 'home'
